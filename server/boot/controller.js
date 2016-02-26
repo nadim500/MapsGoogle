08b5a20709fcfa797e99d677d546c855ee9b2b33
Duivan Mata <duivan5@gmail.com>
2016-02-25 19:22:18 -0500

    marcando mapa

--- server/boot/controller.js
+++ server/boot/controller.js
@@ -4,15 +4,38 @@ module.exports = function(app){
 	var router = app.loopback.Router();
 
 	router.get('/',function(req,res){
-		res.render('mapgoogle');
-	});
-	router.get('/1',function(req,res){
-		res.render('address');
-	});
-	router.get('/2',function(req,res){
-		res.render('formulario');
+		return res.render('mapgoogle');
 	});
 
-	app.use(router);
-	
+	router.get('/1',function(req,res){
+		return res.render('address');
+	});
+
+	router.get('/2',function(req,res){
+		return res.render('formulario');
+	});
+
+	router.get('/3',function(req,res){
+		return res.render('formulario')
+	});
+
+	router.post('/dataGoogle',function(req,res){
+		var data = req.body.autocompletado;
+		data = JSON.parse(data);
+		console.log(typeof(data));
+		console.log('recibido en controller : ', data);
+		var location = data.geometry.location;
+		var pruebaString = JSON.stringify(location)
+		console.log("location : ",location);
+		console.log("pruebaString : ",pruebaString);
+		return res.render('mostrarData',{
+			location : pruebaString
+		});
+	});
+
+	router.get('/mostrarData',function(req,res){
+		return res.render('mostrarData');
+	});
+
+	app.use(router);	
 }
