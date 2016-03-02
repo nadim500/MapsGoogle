0455819dcc731c957fab41bbd5ede119fad0690e
Duivan Mata <duivan5@gmail.com>
2016-03-01 19:09:54 -0500

    iconos añadidos

--- server/boot/controller.js
+++ server/boot/controller.js
@@ -3,6 +3,20 @@ module.exports = function(app) {
     var Stop = app.models.Stop;
     var router = app.loopback.Router();
 
+    router.get('/',function(req,res){
+        res.redirect('/destino');
+    });
+
+    router.get('/rapido',function(req,res){
+        Stop.find({},function(err,objResult_Stop){
+            if(err) return res.sendStatus(404);
+            var string = JSON.stringify(objResult_Stop);
+            return res.render('marcarRapido',{
+                string : string
+            });
+        });
+    });
+
     router.get('/destino', function(req, res) {
         Stop.find({}, function(err, objResult_Stop) {
             if (err) return res.sendStatus(404);
@@ -42,47 +56,6 @@ module.exports = function(app) {
     router.get('/vehiculo/nuevo',function(req,res){
         return res.render('vehiculoNuevo');
     });
-
-
-/*    router.post('/dataGoogle', function(req, res) {
-        var place = req.body.autocompletado;
-        var nombre = req.body.nombre
-        place = JSON.parse(place);
-        console.log(typeof(place));
-        console.log("place: ", place);
-
-        Stop.findOne({
-            where: {
-                place_id: place.place_id
-            }
-        }, function(err, objResult) {
-            console.log("objResult en findOne: ", objResult);
-            if (err) return res.sendStatus(404);
-            else if (objResult == null) {
-                var newStop = {
-                    direccion: place.name,
-                    coordenada: place.geometry.location,
-                    nombre: nombre,
-                    place_id: place.place_id
-                };
-                Stop.create(newStop, function(err, objStop) {
-                    if (err) return res.sendStatus(404);
-                    Stop.find({}, function(err, objResult_Stop) {
-                        if (err) return res.sendStatus(404);
-                        console.log("objResult_Stop: ", objResult_Stop);
-                        var string = JSON.stringify(objResult_Stop);
-                        return res.render('mostrarData', {
-                            objResult_Stop: objResult_Stop,
-                            string: string
-                        });
-                    });
-                });
-            } else {
-                return res.render('formulario');
-            }
-        });
-    });
-*/
     router.post('/principalPost', function(req, res) {
         var place = req.body.autocompletado;
         var nombre = req.body.nombre;
@@ -122,6 +95,47 @@ module.exports = function(app) {
         });
     });
 
+    router.post('/rapidoPost',function(req,res){
+        var place = req.body.autocompletado;
+        var nombre = req.body.nombre;
+        var tipo = req.body.tipo;
+        place = JSON.parse(place);
+        console.log("tipo: ",tipo);
+        console.log(typeof(place));
+        console.log("place: ", place);
+
+        Stop.findOne({
+            where: {
+                place_id: place.place_id
+            }
+        }, function(err, objResult) {
+            console.log("objResult en findOne: ", objResult);
+            if (err) return res.sendStatus(404);
+            else if (objResult == null) {
+                var newStop = {
+                    direccion: place.name,
+                    coordenada: place.geometry.location,
+                    nombre: nombre,
+                    place_id: place.place_id,
+                    tipo : tipo
+                };
+                Stop.create(newStop, function(err, objStop) {
+                    if (err) return res.sendStatus(404);
+                    Stop.find({}, function(err, objResult_Stop) {
+                        if (err) return res.sendStatus(404);
+                        console.log("objResult_Stop: ", objResult_Stop);
+                        var string = JSON.stringify(objResult_Stop);
+                        return res.render('marcarRapido', {
+                            objResult_Stop: objResult_Stop,
+                            string: string
+                        });
+                    });
+                });
+            } else {
+                return res.render('destino');
+            }
+        });
+    })
 
     app.use(router);
 };
