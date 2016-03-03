fb3353ab1d0a2c799e6a9e219642c1c79545c9f8
nadim500 <duivan5@gmail.com>
2016-03-03 04:21:56 +0000

    recojo añadido

--- server/boot/cligo.js
+++ server/boot/cligo.js
@@ -17,6 +17,38 @@ module.exports = function(app) {
     router.get('/motorizado/listar',function(req,res){
         res.render('motorizadoListar');
     });
+    
+    router.get('/recojo',function(req,res){
+        res.render('recojo');
+    });
 
+    router.get('/recojo/crear',function(req,res){
+        res.render('recojoCrear');
+    });
+
+    router.get('/recojo/listar',function(req,res){
+        res.render('recojoListar');
+    });
+
+    router.get('/destino',function(req,res){
+        res.render('destino');
+    });
+
+    router.get('/destino/crear',function(req,res){
+        res.render('destinoCrear');
+    });
+
+    router.get('/destino/listar',function(req,res){
+        res.render('destinoListar');
+    });
+    
+    router.get('/resumen/motorizado',function(req,res){
+        res.render('resumenMotorizado');
+    });
+    
+    router.get('/resumen/recojo',function(req,res){
+        res.render('resumenRecojo');
+    });
+    
     app.use(router);
 };
